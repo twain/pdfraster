09a614ca2b12ac40b6ccb092c49abd4e31877f5d
Mark McLaughlin <mark.mclaughlin@kodakalaris.com>
2017-08-18 09:52:48 -0400

    Added pointer-to-function declarations for the memory management calls...

--- pdfras_writer/PdfAlloc.h
+++ pdfras_writer/PdfAlloc.h
@@ -13,24 +13,29 @@ typedef struct t_pdmempool t_pdmempool;
 // Create and return an allocation pool, which sub-manages
 // memory from the underlying 'os' platform.
 extern struct t_pdmempool *pd_alloc_new_pool(t_OS *os);
+typedef struct t_pdmempool* (PDFRASAPICALL *pfn_pd_alloc_new_pool)(t_OS *os);
 
 // Return a memory block to a pool.
 // ptr = a pointer previously returned by __pd_alloc and not since freed.
 // Or NULL, which is ignored.
 extern void pd_free(void *ptr);
+typedef void (PDFRASAPICALL *pfn_pd_free)();
 
 // Return the number of blocks currently allocated from a pool
 extern size_t pd_get_block_count(t_pdmempool* pool);
+typedef size_t (PDFRASAPICALL *pfn_pd_get_block_count)(t_pdmempool* pool);
 
 // Return the total bytes currently allocated to blocks in a pool.
 // Does not include any overhead, just the sum of the sizes alloc'd and not yet freed.
 extern size_t pd_get_bytes_in_use(t_pdmempool* pool);
-
 extern size_t pd_get_block_size(void* block);
+typedef size_t (PDFRASAPICALL *pfn_pd_get_bytes_in_use)(t_pdmempool* pool);
+typedef size_t (PDFRASAPICALL *pfn_pd_get_block_size)(void* block);
 
 // Frees all the blocks in a pool.
 // Same effect as calling pd_free on every outstanding block in the pool.
 extern void pd_pool_clean(t_pdmempool* pool);
+typedef void (PDFRASAPICALL *pfn_pd_pool_clean)(t_pdmempool* pool);
 
 // (internal) Allocate a block of memory in/from an allocation pool.
 // allocsys = allocation pool to allocate from/in.
@@ -39,21 +44,25 @@ extern void pd_pool_clean(t_pdmempool* pool);
 // returns: pointer to at least nb bytes of memory not in use for anything else,
 // associated with the allocation pool and filled with 0's.
 extern void *__pd_alloc(t_pdmempool *allocsys, size_t nb, char *allocatedBy);
+/* no typedef for this one */
 
 // Return the memory pool that a block was allocated from.
 // If ptr is NULL, returns NULL.
 extern t_pdmempool *pd_get_pool(void *ptr);
+typedef t_pdmempool* (PDFRASAPICALL *pfn_pd_get_pool)(void *ptr);
 
 // (internal) Return a memory block to a pool.
 // ptr = a pointer previously returned by __pd_alloc and not since freed.
 // Or NULL, which is ignored.
 // verifyptr = if truthy and DEBUG build, do extra validation
 extern void __pd_free(void *ptr, pdbool veryifyptr);
+/* no typedef for this one */
 
 // Release an allocation pool.
 // This releases all memory associated with the pool back to the
 // platform underlying the pool.
 extern void pd_alloc_free_pool(t_pdmempool *sys);
+typedef void (PDFRASAPICALL *pfn_pd_alloc_free_pool)(t_pdmempool *sys);
 
 #define ELEMENTS(a) (sizeof(a)/sizeof((a)[0]))
 
